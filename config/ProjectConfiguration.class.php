ce15c0bedd6aab217ebc3b492bdab699bd4a3d09
Tom <tom.camin@gmail.com>
2012-05-09 23:49:19 +0100

    init project

---
+++ config/ProjectConfiguration.class.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once '/Users/tomcamin/Sites/symfony1.4.17/lib/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+  public function setup()
+  {
+    $this->enablePlugins('sfDoctrinePlugin');
+  }
+}
